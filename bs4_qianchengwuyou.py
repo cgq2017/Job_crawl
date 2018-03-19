a11752db5377b7b3dcbc211fb3dfa8d1d552046b
cgq2017 <31460248+cgq2017@users.noreply.github.com>
2018-03-19 21:32:31 +0800

    Update bs4_qianchengwuyou.py

--- bs4_qianchengwuyou.py
+++ bs4_qianchengwuyou.py
@@ -15,11 +15,11 @@ def craw_spider(url):
     '''网页获取'''
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36 QIHU 360SE'
-    }
+    } #请求头
     # html=urlopen(url)
     session=requests.Session()
     html=session.get(url,headers=headers)
-    html=html.text.encode(html.encoding)
+    html=html.text.encode(html.encoding) #需要编码，否则会乱码，因为pytho3默认的是unicode编码
     html=html.decode('gbk')
     bsobi=BeautifulSoup(html,'lxml')
 
@@ -32,17 +32,17 @@ def get_information(url):
     except:
         time.sleep(1)
         m = craw_spider(url)
-    g = m.find('div', {'id': 'resultList'}).find_all('div', {'class': "el"})[1:]
+    g = m.find('div', {'id': 'resultList'}).find_all('div', {'class': "el"})[1:] #得到一页中的招聘信息，有很多标题组成
     a=[]
     for i in range(len(g)):
-        g1 = g[i].findAll('a')
+        g1 = g[i].findAll('a') #获取a元素标签
         # m=''.join(g[i].get_text().strip().split('\n'))
-        m = g[i].get_text().strip().split('\n')   #标题的获取
-        title.append(m[0].strip())
-        company.append(m[-4].strip())
-        destination.append(m[-3].strip())
-        money.append(m[-2].strip())
-        href = g1[0].attrs['href']   #获取工作链接
+        m = g[i].get_text().strip().split('\n')   #得到一个列表，里面有招聘标题，公司名称，在工作地点，薪资
+        title.append(m[0].strip())    #招聘标题
+        company.append(m[-4].strip())   #公司名称
+        destination.append(m[-3].strip())   #工作地点
+        money.append(m[-2].strip())   #薪资
+        href = g1[0].attrs['href']   #获取招聘标题链接
         hef.append(href)
         print(m, href)
         try:
@@ -50,11 +50,11 @@ def get_information(url):
         except:
             try:
                 time.sleep(1)   #防止响应失败
-                content_work = craw_spider(g1[0].attrs['href'])
+                content_work = craw_spider(g1[0].attrs['href']) #再次获取
             except:
-                content_work=''
+                content_work='' #获取失败，返回空
         try:
-            if(len(content_work)==0):
+            if(len(content_work)==0): #判断工作内容是否为空，因为为空的列表无法通过索引获取，否则会报错
                 work_information=' '
             else:
                 w = content_work.find('div', {'class': 'bmsg job_msg inbox'})
@@ -93,17 +93,17 @@ def excel():
         try:
             write_f.writerow((title[i],company[i],destination[i],money[i],hef[i],information[i]))
         except:
-            write_f.writerow((title[i], company[i], destination[i], money[i], hef[i],'######################'))
+            write_f.writerow((title[i], company[i], destination[i], money[i], hef[i],'######################')) #有些工作内容无法获取，没办法转码
     file_name.close()
-    mail("数据信息写入完成")
+    mail("数据信息写入完成") #发送邮件告知，数据采集完成
 def mail(text):
     '''邮件发送'''
     my_sender = 'XXXXX'  # 发件人邮箱账号
-    my_pass = 'XXXXX'  # 发件人邮箱授权密码
+    my_pass = 'XXXXX'  # 发件人邮箱授权密码，注意是开启pop3后的授权密码，不是邮箱的密码
     my_user = 'XXXXXX'  # 收件人邮箱账号，我这边发送给自己
 
     try:
-        msg=MIMEMultipart()
+        msg=MIMEMultipart() #为了创建附件
         content1 = MIMEText(text, 'plain', 'utf-8')
         msg['From'] = formataddr(["FromRunoob", my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
         msg['To'] = formataddr(["FK", my_user])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
@@ -119,7 +119,7 @@ def mail(text):
         server.sendmail(my_sender, [my_user, ], msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
         server.quit()  # 关闭连接
         print("邮件发送成功")
-    except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+    except Exception: 
         print("邮件发送失败")
 
 title=[]
